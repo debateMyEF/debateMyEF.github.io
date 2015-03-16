501b85c71840872f707b96a0bfb25498000ff1f4
Sebastian Osorio <svzosorio@boolinc.co>
2015-03-15 22:21:12 -0500

    1. Install string-formatter 2. Delete the use of labels 3. Create a angular filter 4. Implement the new filter

--- js/app.js
+++ js/app.js
@@ -11,17 +11,35 @@ angular
         var times = $ngCountdown.units.hours | $ngCountdown.units.minutes
         | $ngCountdown.units.seconds | $ngCountdown.units.days;
 
-        $ngCountdown.setLabels(
-            ' milisegundo| segundo| minuto| hora| día| semana| mes| año| década| século| milenio',
-            ' milisegundos| segundos| minutos| horas| días| semanas| meses| años| décadas| séculos| milenios',
-            ' y ',
-            ' , ',
-            'ahora');
+        $scope.date = $ngCountdown.init(now, final, times)
 
-            $scope.timestamp = $ngCountdown.init(now, final, times).toString();
+        $timeout(init, 1000);
+    };
 
-            $timeout(init, 1000);
-        };
+    init();
+})
+.filter('timestamp', function() {
+  return function(input, type) {
 
-        init();
-    });
+    var labels = {
+        day: function(days){
+            if(days == 1) return '{0} día'.format(days);
+            else return '{0} días'.format(days);
+        },
+        hour: function(hours){
+            if(hours == 1) return '{0} hora'.format(hours);
+            else return '{0} horas'.format(hours);
+        },
+        minute: function(minutes){
+            if(minutes == 1) return '{0} minuto'.format(minutes);
+            else return '{0} minutos'.format(minutes);
+        },
+        second: function(seconds){
+            if(seconds == 1) return '{0} segundo'.format(seconds);
+            else return '{0} segundos'.format(seconds);
+        }
+    };
+
+    return labels[type](input);
+  };
+});
